23a6d9b9340c3d8aab1dfe9ea080bca22dbbcefa
Ricardo Rosiles <brayan.rosiles@grupojaque.com>
2020-09-22 00:20:24 -0500

    ADD DOCS

---
+++ docs/precache-manifest.5586f12ce08182ab298fb4116f92d801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c399bd8b05e99dc3bb1a5e96be69195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5315f3555dd2ffef6c4e",
+    "url": "/static/css/main.b7458d60.chunk.css"
+  },
+  {
+    "revision": "d8d64d42dd82186346d3",
+    "url": "/static/js/2.de44a5c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de44a5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5315f3555dd2ffef6c4e",
+    "url": "/static/js/main.825c3b40.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
